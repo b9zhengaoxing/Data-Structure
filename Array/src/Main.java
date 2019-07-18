8a2ee1e5ad5587da4650cd8d973259e43ccf4d3e
tengfei li <litengfei@qiyi.com>
2019-07-18 15:05:19 +0800

    Unit2 Array 使用Java中的数组
    1. Java中的数组，只能存放同一类型的数据，
    2. Java中的数组，开辟空间&&直接赋值创建
    3. Java for(int score: scores ) 语法便利

--- Array/src/Main.java
+++ Array/src/Main.java
@@ -1,7 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    
+        //数组，一队数据排成排
+        //索引，0 —— N
+        //对于Java语言 ,数组中只能存放同一种类型的数据
+
+        /*声明1:开辟空间，声明大小*/
+        int[] arr = new int[10];//new 开辟空间，同时必须声明数组可以装多少个元素
+        for(int i = 0 ; i < arr.length/*比使用硬编码容易修改*/ ; i ++){
+             arr[i] = i;
+        }
+
+        /*声明时有初始值*/
+        int[] scores = new int[]{99,100,101};
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i]);
+        }
+
+        scores[0] = 19;
+        /*Foreach,可遍可迭代*/
+        for (int score:scores) {
+            System.out.println(score);
+        }
     }
 }

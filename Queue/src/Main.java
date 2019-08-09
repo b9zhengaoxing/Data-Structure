66cbd96efd2b25b1aec782ca71f0d94d66261471
tengfei li <litengfei@qiyi.com>
2019-08-09 16:14:57 +0800

    Unit 3 栈和队列 循环队列LoopQueue 和 ArrayQueue时间复杂度的对比

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,6 +1,33 @@
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private static double testQueue (Queue<Integer> q, int opCount) {
+
+        long startTime = System.nanoTime();
+
+        Random radom = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.enqueue(radom.nextInt(Integer.MAX_VALUE));/*从0到最大*/
+        }
+        for (int i = 0; i < opCount; i++) {
+            q.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+        return (endTime - startTime) /1000000000.0;
+    }
+
+    public static void main (String[] args) {
+        // write your code here
+        int opCount = 100000;
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue is " + time1 + " s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue is " + time2 + " s");
     }
 }

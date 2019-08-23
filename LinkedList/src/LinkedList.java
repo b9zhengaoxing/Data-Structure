4af95f4009352587566ec2cdb9ecac69002a4688
tengfei li <litengfei@qiyi.com>
2019-08-23 10:30:28 +0800

    Unit 4 链表 增加
    1. 对比：链表在头增加容易，数组在尾增加容易
    2. 关于index，size 比index上限要大1，所以可以有add(index)这种操作
    3. 增加 prev.next = new Node(e,prev.next);
    4. 对于index = 0 的特殊操作，因为Index = 0，，没有prev所有需要特殊操作

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -23,4 +23,83 @@ public class LinkedList<E> {
             return e.toString();
         }
     }
+
+    private Node head;
+    private int size; /*private,只能内部修改*/
+
+    public LinkedList () {
+        head = null;
+        size = 0;
+    }
+
+    /**
+     * 获取链表中元素个数
+     *
+     * @return
+     */
+    public int getSize () {
+        return size;
+    }
+
+    /**
+     * 链表是否为空
+     *
+     * @return
+     */
+    public boolean isEmpty () {
+        return size == 0;
+    }
+
+    /**
+     * 在链表头部增加元素
+     * @param e
+     */
+    public void addFirst(E e){
+
+//        if (e == null){ /*因为这是数据，所以为空也没关系*/
+//            throw new IllegalArgumentException("addFirst error，e is Empty!");
+//        }
+
+//        Node node = new Node(e,this.head);
+//        this.head = node;
+        head = new Node(e, head);
+        size ++;
+    }
+
+
+    /**
+     * 在链表的index（0-based）位置添加新元素e
+     * 并非链表常规操作，练习用
+     * @param index
+     * @param e
+     */
+    public void add(int index,E e){
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+        }
+
+        if (index == 0){
+            this.addFirst(e); /*对链表头节点的特殊处理*/
+        }else {
+            /*链表的循环*/
+            Node prev = this.head;
+            for (int i = 0; i < index - 1/*找到之前的*/; i++) {
+                prev = prev.next;
+            }
+//        Node node = new Node(e);
+//        node.next = prev.next;
+//        prev.next = node;
+
+            prev.next = new Node(e,prev.next);
+            size ++;
+        }
+    }
+
+    /**
+     * 向链表尾部增加一个元素
+     * @param e
+     */
+    public void addLast(E e){/*这里size 比index上限大一所以可以添加*/
+        add(size - 1, e);
+    }
 }

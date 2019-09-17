20c6ca56f648d7b171a35bd0016f770a404aaa05
tengfei li <litengfei@qiyi.com>
2019-09-17 14:18:30 +0800

    Unit 4 链表 遍历，修改
    1. 插入循环，从 dummyHead开始遍历，找到前一个节点
    2. 修改index，从dunmmyHead.next开始遍历，找到index
    3. for循环顺序， for（1，2，4）{3}
    4. 三种遍历
    	while (cur.next != null)
    	for (Node cur = dummyHead.next ; cur.next != null ; cur = cur.next)
    	for (int i = 0; i < size - 1; i++)

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -5,21 +5,21 @@ public class LinkedList<E> {
         public E e;
         public Node next;
 
-        public Node(E e,Node next){
+        public Node (E e, Node next) {
             this.e = e;/*变量名重名，所以用要this.代表节点的e*/
             this.next = next;
         }
 
-        public Node(E e){
-            this(e,null);
+        public Node (E e) {
+            this(e, null);
         }
 
-        public Node(){
-            this(null,null);
+        public Node () {
+            this(null, null);
         }
 
         @Override
-        public String toString(){
+        public String toString () {
             return e.toString();
         }
     }
@@ -28,7 +28,7 @@ public class LinkedList<E> {
     private int size; /*private,只能内部修改*/
 
     public LinkedList () {
-        dummyHead = new Node(null,null);
+        dummyHead = new Node(null, null);
         size = 0;
     }
 
@@ -52,40 +52,154 @@ public class LinkedList<E> {
 
     /**
      * 在链表头部增加元素
+     *
      * @param e
      */
-    public void addFirst(E e){
+    public void addFirst (E e) {
 
-        this.add(0,e);
+        this.add(0, e);
     }
 
 
     /**
      * 在链表的index（0-based）位置添加新元素e
      * 并非链表常规操作，练习用
+     *
      * @param index
      * @param e
      */
-    public void add(int index,E e){
+    public void add (int index, E e) {
         if (index < 0 || index > size) {
             throw new IllegalArgumentException("Add failed. Illegal index.");
         }
 
-        /*链表的循环*/
+        /*链表的循环，遍历index前一个位置*/
         Node prev = this.dummyHead;
-        for (int i = 0; i < index - 1/*找到之前的*/; i++) {
+        for (int i = 0; i < index/*找到之前的*/; i++) {
             prev = prev.next;
         }
-        prev.next = new Node(e,prev.next);
-        size ++;
+        prev.next = new Node(e, prev.next);
+        size++;
 
     }
 
     /**
      * 向链表尾部增加一个元素
+     *
      * @param e
      */
-    public void addLast(E e){/*这里size 比index上限大一所以可以添加*/
+    public void addLast (E e) {/*这里size 比index上限大一所以可以添加*/
         add(size - 1, e);
+    }
+
+    /**
+     * 获得链表第index（0 - Based）个位置第元素
+     *
+     * @param index 不常用用来做练习
+     * @return
+     */
+    public E get (int index) {
+        if (index < 0 || index >= size/**/) {
+            throw new IllegalArgumentException("Get Failed. Illegal index.");
+        }
+
+        Node cur = dummyHead.next/*从第一个元素开始遍历*/;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    /**
+     * 获取链表第一个元素
+     *
+     * @return
+     */
+    public E getFirst () {
+        return get(0);
+    }
+
+    /**
+     * 获取链表最后一个元素
+     *
+     * @return
+     */
+    public E getLast () {
+        return get(size - 1);
+    }
+
+    /**
+     * 修改链表index位置的元素，（0——base)个位置的元素为e,非常规操作，练习用
+     * @param index
+     * @param e
+     */
+    public void set(int index,E e){
+
+        if (index < 0 || index >= size/**/) {
+            throw new IllegalArgumentException("Set Failed. Illegal index.");
+        }
+
+        /*反馈：我要遍历到index，是一个终点*/
+//        Node cur = dummyHead;
+//        for (int i = 0; i <= index; i++) {
+//            cur = cur.next;
+//            if (i == index) cur.e = e;
+//        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    /**
+     * 查找链表中是否存在元素 e
+     * @return
+     */
+    public boolean contains (E e) {
+
+        /*反馈：要对每一个状态进行遍历*/
+        Node cur = dummyHead.next;
+
+//        for (int i = 0; i < size - 1 ; i++) {
+//            if (cur.e.equals(e)) {
+//                return true;
+//            }
+//            cur = cur.next;
+//        }
+
+        //写法2
+        while (cur.next != null){
+
+            if (cur.equals(e)){
+                return true;
+            }
+            cur = cur.next;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString(){
+
+        StringBuilder res = new StringBuilder();
+
+        //写法2
+//        Node cur = dummyHead.next;
+//        while (cur.next != null){
+//
+//            res.append(cur + "->");
+//            cur = cur.next;
+//        }
+
+        //  for循环顺序， for（1，2，4）{3}
+        for (Node cur = dummyHead.next ; cur.next != null ; cur = cur.next) {
+            res.append(cur + "->");
+        }
+
+        res.append("NULL");
+
+        return res.toString();
     }
 }

5363b2355b56653b4bf93809bc36d59fe34b341d
tengfei li <litengfei@qiyi.com>
2019-11-21 16:52:30 +0800

    Unit 6 BST 递归增加二分搜索树节点
    1. 对象类型不能直接比较，compareTo
    2. Size ++ 总是忘记

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -1,4 +1,4 @@
-public class BST<E extends Comparable<E>> {/*可以比较*/
+public class BST<E extends Comparable<E>> {/* 二分搜索树，需要是可以比较*/
 
     private class Node {
         E e;
@@ -19,11 +19,52 @@ public class BST<E extends Comparable<E>> {/*可以比较*/
         size = 0;
     }
 
-    public int size(){
+    public int size () {
         return size;
     }
 
-    public Boolean empty(){
+    public Boolean empty () {
         return size == 0;
     }
+
+    public void add (E e) {
+        if (root == nil) {
+            root = new Node(e);
+            size++; //总是忘了size的
+        } else {
+            add(root, e);
+        }
+    }
+
+    /**
+     * 向二分搜索树插入新节点
+     * @param node 此刻递归的root节点
+     * @param e 
+     */
+    private void add (Node node, E e) {
+
+        if (root.e.equals(e)) {
+            return;
+        }
+        //对象类型不能直接比较
+        else if (root.e.compareTo(e) > 0 && root.left == nil) {
+            root.left = new Node(e);
+            size++;
+            return;
+        } else if (root.e.compareTo(e) < 0 && root.right == nil) {
+            root.right = new Node(e);
+            size++;
+            return;
+        }
+
+        if (root.e.compareTo(e) > 0) {
+            this.add(root.left, e);
+            return;
+        } else if (root.e.compareTo(e) < 0) {
+            this.add(root.right, e);
+            return;
+        }
+
+    }
+
 }

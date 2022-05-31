c690d933132bae7e03d75d0eaef6355571b27db0
litengfei <1164834201@qq.com>
2022-05-31 10:42:47 +0800

    Unit2-Stack-Queue 3 Valid Parentheses -- LeetCode
    1. 错题本
      1.1 else  操作 Or return
      1.2 LeetCode test case
    2. Inner Class 语法
      2.1 写在类里面
      2.2 不允许有static
    3. 关于LeetCode 不必完美，目的是学会
      3.1 result - oriented
      3.2 海外经历Google

---
+++ Unit2-Stack-Queue/src/Solution.java
@@ -0,0 +1,61 @@
+//leetcode 20
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+//        An input string is valid if:
+//
+//        Open brackets must be closed by the same type of brackets.
+//        Open brackets must be closed in the correct order.
+//
+//
+//        Example 1:
+//
+//        Input: s = "()"
+//        Output: true
+//        Example 2:
+//
+//        Input: s = "()[]{}"
+//        Output: true
+//        Example 3:
+//
+//        Input: s = "(]"
+//        Output: false
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            Character character = s.charAt(i);
+            if (character.equals('(') || character.equals('[') || character.equals('{')) {
+                stack.push(character);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+                if (character.equals(')') && stack.peek().equals('(')) {
+                    stack.pop();
+                } else if (character.equals(']') && stack.peek().equals('[')) {
+                    stack.pop();
+                }else if (character.equals('}') && stack.peek().equals('{')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty() == true;
+    }
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        String test_string1 = "(])";
+        String test_string2 = "()[]{}";
+        String test_string3 = "(]";
+
+        System.out.println(test_string1 + solution.isValid(test_string1));
+        System.out.println(test_string2 + solution.isValid(test_string2));
+        System.out.println(test_string3 + solution.isValid(test_string3));
+    }
+}

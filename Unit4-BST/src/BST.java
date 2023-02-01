e8441a9bb01bb3df70704ac4a86f67f8e5548a50
litengfei <1164834201@qq.com>
2023-02-01 11:48:03 +0800

    Unit4-BST - 1. BST 递归 前序中序后序
    一.树 —— 结构 高效
    
    0. Why？高效
      0.1 高效 处理一类数据（图书馆，组织架构）
    1. Tree
      1.1 root leaf subtree
      1.2 null 也是一棵树
    2. 二叉树
      2.1 两个分叉
      2.2 满
      2.3 每一个子树都是二叉树
    3. BST binary search tree
      3.1 左 < 右
      3.2
    
    递归
    
    1. 思想：问题分治
      1.1 按节点拆分问题
      2.2 先写跳出条件 return
        if else 写全
    2. add
      null 也看做节点子问题 可以简化
    3. contain
      if else 不重不漏
    
    遍历 根
    1. 前序遍历
    2. 中序遍历 —— 完成排序
    3. 后续遍历

--- Unit4-BST/src/BST.java
+++ Unit4-BST/src/BST.java
@@ -1,16 +1,17 @@
+import java.lang.reflect.Array;
+
 public class BST<E extends Comparable> {
 
     // private 内部结构 Node 封装，给出外部操作
     //
     private class Node {
         public E data;
-        public Node leftLeaf;
-        public Node rightLeaf;
+        public Node left,right; //同一类型写在一起
 
         public Node(E e) {
             data = e;
-            leftLeaf = null;
-            rightLeaf = null;
+            left = null;
+            right = null;
         }
     }
 
@@ -24,7 +25,7 @@ public class BST<E extends Comparable> {
     }
 
     public boolean isEmpty() {
-        return true ? false : size == 0;
+        return size == 0 ? true : false;
     }
 
     public int getSize() {
@@ -34,8 +35,9 @@ public class BST<E extends Comparable> {
     //递归：问题分解思想
     public void add(E e) {
 
-        if (this.isEmpty()) {
+        if (root == null) {
             root = new Node(e);
+            size++;
         } else {
             this.add(root, e);
         }
@@ -44,28 +46,34 @@ public class BST<E extends Comparable> {
     // Overload:函数重载，
     private void add(Node node, E e) {
 
-        //递归： 返回条件
+        //相等是重要的终止条件
+        if (node.data == e)
+            return;
+
+        //递归： 先写返回条件
         if (node == null) {
             return;
-        } else if (e.compareTo(node.data) < 0 && node.leftLeaf == null) {
-            node.leftLeaf = new Node(e);
+        } else if (e.compareTo(node.data) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
             return;
-        } else if (e.compareTo(node.data) > 0 && node.rightLeaf == null) {
-            node.rightLeaf = new Node(e);
+        } else if (e.compareTo(node.data) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
             return;
         }
 
-        if (e.compareTo(node.data) < 0 && node.leftLeaf != null) {
-            this.add(node.leftLeaf, e);
+        if (e.compareTo(node.data) < 0 && node.left != null) {
+            this.add(node.left, e);
         }
 
-        if (e.compareTo(node.data) > 0 && node.rightLeaf != null) {
-            this.add(node.rightLeaf, e);
+        if (e.compareTo(node.data) > 0 && node.right != null) {
+            this.add(node.right, e);
         }
     }
 
     public void add_fix(E e) {
-        // null 也是 二叉树
+        // null 也是 二叉树 递归到下一层
         root = this.add_fix(root, e);
     }
 
@@ -75,18 +83,134 @@ public class BST<E extends Comparable> {
 
         //设置返回条件
         if (node == null) {
+            size++;
             return new Node(e);
-        } else if (e.compareTo(node.data) < 0 ) {
-            return add_fix(node.leftLeaf,e);
-        } else if (e.compareTo(node.data) > 0 ) {
-            return add_fix(node.rightLeaf,e);
         }
 
-        return null;
+        if (e.compareTo(node.data) < 0) {
+            node.left = add_fix(node.left, e);
+        } else if (e.compareTo(node.data) > 0) {
+            node.right = add_fix(node.right, e);
+        }
+
+        //相等时候直接返回，考虑在其中了
+        return node;
+    }
+
+    public boolean contain(E e) {
+        return this.contain(root, e);
+    }
+
+    private boolean contain(Node node, E e) {
+
+        if (node.data.equals(e)) {
+            return true;
+        }
+//        if (node == null)
+//            return false;
+
+//        if (node.data.compareTo(e) < 0) {
+//            this.contain(node.left, e);
+//        } else if (node.data.compareTo(e) < 0) {
+//            this.contain(node.right, e);
+//        }
+//
+//        return false;
+
+        //if else 写完整 —— 可以避免遗漏
+        if (node.data.compareTo(e) < 0) {
+            return this.contain(node.left, e);
+        } else if (node.data.compareTo(e) < 0) {
+            return this.contain(node.right, e);
+        } else
+            return false;
+    }
+
+    public void preOrder() {
+        this.preOrder(root);
+    }
+
+    //中顺遍历 —— 天然排序
+    public void inOrder() {
+        this.inOrder(root);
+    }
+
+    public void postOrder() {
+        this.postOrder(root);
+    }
+
+    //前序遍历
+    private void preOrder(Node node) {
+        if (node == null)
+            return;
+
+        System.out.println(node.data);
+        this.preOrder(node.left);
+        this.preOrder(node.right);
+    }
+
+    private void inOrder(Node node) {
+        if (node == null)
+            return;
+
+        this.inOrder(node.left);
+        System.out.println(node.data);
+        this.inOrder(node.right);
+    }
+
+    private void postOrder(Node node) {
+        if (node == null)
+            return;
+
+        this.postOrder(node.left);
+        this.postOrder(node.right);
+        System.out.println(node.data);
     }
 
     @Override
     public String toString() {
-        return super.toString();
+
+        StringBuilder res = new StringBuilder();
+        res = this.generatePreOrderBST(root,0,res);
+        return res.toString();
+    }
+
+    private StringBuilder generatePreOrderBST(Node node , int deep , StringBuilder str) {
+
+        if (node == null)
+            return str;
+
+        String level_str = "--";
+        int level_int = 0;
+        for (int i = 0; i < deep; i++) {
+            str.append(level_str);
+        }
+
+        deep ++; // ++
+
+        str.append(node.data.toString());
+        str.append("\n");
+        str = this.generatePreOrderBST(node.left,deep,str);
+        str = this.generatePreOrderBST(node.right,deep,str);
+
+        return str;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5,3,6,8,4,2};
+        BST<Integer> binaryTree = new BST();  //泛型，非基础类型，要使用对象实现
+        System.out.println("Empty："+ binaryTree.isEmpty());
+        for (int i :array) {
+            binaryTree.add_fix(i);
+        }
+
+        System.out.println("Empty："+ binaryTree.isEmpty());
+        System.out.println("Size："+ binaryTree.getSize());
+
+//        binaryTree.preOrder();
+//        binaryTree.inOrder();
+        binaryTree.postOrder();
+
+        System.out.println(binaryTree.toString());
     }
 }
